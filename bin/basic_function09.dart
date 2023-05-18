24afa9e27bc783ea3209c8e36e903192211f815b
Mirjalol <jalolubaydullayev575@gmail.com>
2023-05-18 23:34:16 +0500

    09

--- bin/basic_function09.dart
+++ bin/basic_function09.dart
@@ -1,1 +1,7 @@
 // Create a function called `getFavoriteColor` that returns a `String`. The function should return your favorite color.
+String getFavoriteColor(){
+    return 'sariq';
+}
+void main(){
+    print(getFavoriteColor());
+}
