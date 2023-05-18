08c4ddbb301cb651a8586f9626598cd859b142e7
Mirjalol <jalolubaydullayev575@gmail.com>
2023-05-18 23:34:36 +0500

    10

--- bin/basic_function10.dart
+++ bin/basic_function10.dart
@@ -1,1 +1,7 @@
 // Create a function called `isAdult` that returns a `bool`. The function should check if a person is an adult (18 years or older) and return `true` if they are, and `false` otherwise.
+bool isAdult(){
+    return true; 
+}
+void main(){
+    print(isAdult());
+}
