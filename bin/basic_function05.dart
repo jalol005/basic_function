2b1e452caa863a38ca43c2aeb68183fa1464aab1
Mirjalol <jalolubaydullayev575@gmail.com>
2023-05-18 23:33:06 +0500

    05

--- bin/basic_function05.dart
+++ bin/basic_function05.dart
@@ -1,1 +1,7 @@
 // Create a function called `getFullName` that returns a `String`. The function should return your full name.
+String getFullName(){
+    return 'mirjalol';
+}
+void main(){
+    print(getFullName());
+}
